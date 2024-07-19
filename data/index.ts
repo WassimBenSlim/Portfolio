a4d2e784245066fc76cce3ac0a07b58396dd8122
WassimBenSlim <wisskjkj@gmail.com>
2024-07-19 23:36:28 +0100

    data folder

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems = [
+    {title: 'Title1', description:'Desctiption1', id:1},
+    {title: 'Title1', description:'Desctiption1', id:1},
+    {title: 'Title1', description:'Desctiption1', id:1},
+    {title: 'Title1', description:'Desctiption1', id:1},
+]
